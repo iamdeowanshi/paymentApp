d8608b13e3c001eec4140fda86fb2b7c49623fe1
Arnold Laishram <arnold.laishram@tecsolsoftware.com>
2016-03-02 18:27:04 +0530

    Implentation: Bank Info fragment, Location Info Fragment, Bank Info Fragment

--- app/src/main/java/com/batua/android/ui/fragment/MerchantBasicInfoFragment.java
+++ app/src/main/java/com/batua/android/ui/fragment/MerchantBasicInfoFragment.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.batua.android.R;
 import com.batua.android.app.base.BaseFragment;
+
+import butterknife.Bind;
 
 /**
  * Created by febinp on 02/03/16.
  */
 public class MerchantBasicInfoFragment extends BaseFragment {
+
+    @Bind(R.id.spinner_merchant_category) Spinner spinner;
+
+    private View view;
 
     public MerchantBasicInfoFragment() {
 
@@ -20,6 +28,24 @@ public class MerchantBasicInfoFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_merchant_basic_info, null);
+        view = inflater.inflate(R.layout.fragment_merchant_basic_info, null);
+
+        onViewCreated(view,null);
+
+        loadSpinner();
+
+        return view;
     }
+
+    private void loadSpinner() {
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.merchant_category, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+    }
+
+
 }
