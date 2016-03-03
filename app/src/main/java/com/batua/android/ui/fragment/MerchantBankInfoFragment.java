c33798095bb1acd6572ad9b0e0dd7d3e94be71cb
Arnold Laishram <arnold.laishram@tecsolsoftware.com>
2016-03-03 13:42:06 +0530

    Implementation for custom layout of spinner, list_merchant_status layout, Edit Merchant Activity.

--- app/src/main/java/com/batua/android/ui/fragment/MerchantBankInfoFragment.java
+++ app/src/main/java/com/batua/android/ui/fragment/MerchantBankInfoFragment.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.batua.android.R;
 import com.batua.android.app.base.BaseFragment;
+import com.batua.android.ui.custom.LoadSpinner;
+
+import butterknife.Bind;
 
 /**
  * Created by febinp on 02/03/16.
  */
 public class MerchantBankInfoFragment extends BaseFragment {
+
+    @Bind(R.id.spinner_bank)
+    Spinner spinnerBank;
+
+    private View view;
 
     public MerchantBankInfoFragment() {
 
@@ -20,6 +30,13 @@ public class MerchantBankInfoFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_merchant_bank_info, null);
+        view = inflater.inflate(R.layout.fragment_merchant_bank_info, null);
+
+        onViewCreated(view, null);
+
+        LoadSpinner.loadSpinner(getContext(), R.array.merchant_bank, spinnerBank);
+
+        return view;
     }
+
 }
--- app/src/main/java/com/batua/android/ui/fragment/MerchantLocationInfoFragment.java
+++ app/src/main/java/com/batua/android/ui/fragment/MerchantLocationInfoFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.batua.android.R;
 import com.batua.android.app.base.BaseFragment;
+import com.batua.android.ui.custom.LoadSpinner;
+
+import butterknife.Bind;
 
 /**
  * Created by febinp on 02/03/16.
  */
 public class MerchantLocationInfoFragment extends BaseFragment{
+
+    @Bind(R.id.spinner_city) Spinner spinnerCity;
+
+    private View view;
 
     public MerchantLocationInfoFragment() {
 
@@ -20,6 +29,13 @@ public class MerchantLocationInfoFragment extends BaseFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_merchant_location_info, null);
+        view = inflater.inflate(R.layout.fragment_merchant_location_info, null);
+
+        onViewCreated(view,null);
+
+        LoadSpinner.loadSpinner(getContext(), R.array.merchant_city, spinnerCity);
+
+        return view;
     }
+
 }
