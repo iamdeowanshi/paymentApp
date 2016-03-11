3f3fc23159066c531e4466200d3ad49f1ecb18b4
Aaditya Deowanshi <aaditya.deowanshi@tecsolsoftware.com>
2016-03-11 13:38:05 +0530

    Created login and signup screen with forgot and verification screen

---
+++ app/src/main/java/com/batua/android/user/ui/activity/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.batua.android.user.ui.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.batua.android.user.R;
+import com.batua.android.user.app.base.BaseActivity;
+import com.batua.android.user.ui.adapter.LoginFragmentPagerAdpater;
+
+import butterknife.Bind;
+
+public class LoginActivity extends BaseActivity {
+
+    @Bind(R.id.home_tab_layout) TabLayout loginTabLayout;
+    @Bind(R.id.home_viewpager) ViewPager loginViewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loadFragments();
+    }
+
+    private void loadFragments() {
+        loginViewpager.setAdapter(new LoginFragmentPagerAdpater(getSupportFragmentManager(), this));
+        loginTabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                loginTabLayout.setupWithViewPager(loginViewpager);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/batua/android/user/ui/fragment/LoginFragment.java
@@ -0,0 +1,93 @@
+package com.batua.android.user.ui.fragment;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.batua.android.user.R;
+import com.batua.android.user.app.base.BaseFragment;
+import com.batua.android.user.ui.activity.MobileNumberActivity;
+import com.batua.android.user.util.Bakery;
+import com.batua.android.user.util.ViewUtil;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @author Aaditya Deowanshi.
+ */
+public class LoginFragment extends BaseFragment {
+
+    @Inject Bakery bakery;
+
+    @Bind(R.id.edt_email) EditText edtEmail;
+    @Bind(R.id.input_layout_email) TextInputLayout inputLayoutEmail;
+    @Bind(R.id.input_password) EditText inputPassword;
+    @Bind(R.id.input_layout_password) TextInputLayout inputLayoutPassword;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, null);
+        injectDependencies();
+        return view;
+    }
+
+    @OnClick({R.id.txt_forgot_password, R.id.btn_login, R.id.btn_fb, R.id.btn_gplus})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.txt_forgot_password:
+                startActivity(MobileNumberActivity.class, null);
+                break;
+            case R.id.btn_login:
+                validate();
+                break;
+            case R.id.btn_fb:
+                bakery.snackShort(getContentView(), "Loged In , Home screen coming soon");
+                break;
+            case R.id.btn_gplus:
+                bakery.snackShort(getContentView(), "Loged In , Home screen coming soon");
+                break;
+        }
+    }
+
+    private void validate() {
+        ViewUtil.hideKeyboard(getContentView());
+        boolean isValid = isValidEmail(edtEmail.getText()) || isValidNumber(edtEmail.getText());
+
+        if (isValid) {
+            bakery.snackShort(getContentView(), "Loged In , Home screen coming soon");
+            inputLayoutEmail.setErrorEnabled(false);
+
+            return;
+        }
+
+        inputLayoutEmail.setErrorEnabled(true);
+        inputLayoutEmail.setError("Invalid email or mobile number");
+    }
+
+
+    private final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        }
+
+        return Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    private final static boolean isValidNumber(CharSequence target) {
+        if (target == null || target.length() != 10) {
+            return false;
+        }
+
+        return Patterns.PHONE.matcher(target).matches();
+    }
+
+}
